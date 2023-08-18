37f6f2b5a7978fb9df7442306461fc607edf0dfd
adrian1lara <adrianlara203@gmail.com>
2023-08-18 00:49:10 -0500

    update files generated in dis/

---
+++ dist/534.main.js
@@ -0,0 +1,1 @@
+"use strict";(self.webpackChunkodin_project_restaurant_page=self.webpackChunkodin_project_restaurant_page||[]).push([[534],{534:(e,p,a)=>{e.exports=a.p+"02995f08af4c62f01712.png"}}]);
---
+++ dist/901.main.js
@@ -0,0 +1,1 @@
+"use strict";(self.webpackChunkodin_project_restaurant_page=self.webpackChunkodin_project_restaurant_page||[]).push([[901],{901:(e,a,p)=>{e.exports=p.p+"1a6ae96b78ffd966d828.png"}}]);
